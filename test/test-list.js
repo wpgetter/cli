2ccc23ed27facd4bc297ad00955192f240dead8e
Nabil Kadimi <nabil@kadimi.com>
2017-11-05 23:54:33 +0000

    Testing

--- test/test-list.js
+++ test/test-list.js
@@ -15,8 +15,4 @@ describe('list', () => {
     .catch(error => {throw error;})
   );
 
-  it('should have some results when called without arguments', () => tests.noArgs
-    .then(result => expect(result).to.have.length.above(0))
-    .catch(error => {throw error;})
-  );
 });
