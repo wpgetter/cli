6cc595f0be34102c8e2d91abc283ee91cf8b3807
Nabil Kadimi <nabil@kadimi.com>
2017-11-05 14:06:57 +0000

    Rename `list` tests

--- test/test-list.js
+++ test/test-list.js
@@ -7,9 +7,9 @@ describe('wpgetter', () => {
   describe('list', () => {
 
     let testNoArgs     = Promise.resolve(list.parse());
-    let test = Promise.resolve(list.parse(['name', 'unlikely-to-have-ever-existed']));
+    let testInexistent = Promise.resolve(list.parse(['name', 'unlikely-to-have-ever-existed']));
 
-    it('should have no result for inexistent software', () => test
+    it('should have no result for inexistent software', () => testInexistent
       .then(result => expect(result).to.have.a.lengthOf(0))
       .catch(error => {throw error;})
     );
