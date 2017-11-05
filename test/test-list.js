f7c735ce7f568e823a106a0eca7898b64b239873
Nabil Kadimi <nabil@kadimi.com>
2017-11-05 14:06:15 +0000

    Add test for `list`

--- test/test-list.js
+++ test/test-list.js
@@ -6,10 +6,16 @@ let list = require('../lib/list.js');
 describe('wpgetter', () => {
   describe('list', () => {
 
+    let testNoArgs     = Promise.resolve(list.parse());
     let test = Promise.resolve(list.parse(['name', 'unlikely-to-have-ever-existed']));
 
     it('should have no result for inexistent software', () => test
       .then(result => expect(result).to.have.a.lengthOf(0))
+      .catch(error => {throw error;})
+    );
+
+    it('should have some results when called without arguments', () => testNoArgs
+      .then(result => expect(result).to.have.length.above(0))
       .catch(error => {throw error;})
     );
   });
