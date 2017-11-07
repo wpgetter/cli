65c7ba9093062d011cf733ad572127e262c36424
Nabil Kadimi <nabil@kadimi.com>
2017-11-07 13:06:05 +0000

    Refactor

--- src/init.js
+++ src/init.js
@@ -4,19 +4,7 @@ exports = module.exports = {};
 
 let homeDir       = require('home-dir');
 let mkdirpPromise = require('mkdirp-promise');
-// let term          = require('terminal-kit').terminal;
-
-let wpgetterHomeDir = homeDir('/.wpgetter');
-
-let createwpgetterDirPromise = function (dir, opts, cb) {
-  dir = wpgetterHomeDir;
-  return mkdirpPromise(dir, opts, cb);
-};
 
 exports.parse = function () {
-  return Promise.all([createwpgetterDirPromise()]).then((dir, opts, cb) => {
-    if (dir == wpgetterHomeDir) {
-      // term('^g✓^ The directory ^_%s^ was created successfully.\n', wpgetterHomeDir);
-    }
-  });
+  return mkdirpPromise(homeDir('/.wpgetter'));
 };
